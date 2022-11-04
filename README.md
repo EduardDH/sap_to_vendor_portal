a1b0a0ebfc502d20a808a05114b3951be055a1c6
EduardDH <eduard.zaynullin@deliveryhero.com>
2022-11-04 22:22:01 +0100

    CSV input, JSON file output

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Extract SD Order data from SAP, map to the Vendor Portal fields
 https://docs.google.com/spreadsheets/d/18r3Q1FZeU4SiIt4v3xzZPriZaHYaQ0I68YPOd-fSu9Q
+
+
+How to use?
+
+Put order_ids to the file "order_list.csv" located at the same folder as a script. One order_id per line.
+
+Execute the script. Find the result in the folder "output" in JSON format, one file per oder.
--- get_order_data.py
+++ get_order_data.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
-import logging
-import time
 from hdbcli import dbapi
 from hdbcli.dbapi import Error, ProgrammingError
-#import config
 import json
-from datetime import datetime
+import logging
+import csv
+
 
 global conn
 global cursor
@@ -44,140 +43,149 @@ def main_program():
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
     )
+    # Read input.csv
+    with open('order_list.csv', newline='') as f:
+        reader = csv.reader(f)
+        order_list = list(reader)
 
     # HANA DB Connection
     hana_db_connect(config)
     data = {}
 
     try:
-        # VBRP can have multyple records
-        cursor.execute("SELECT VBAK.ZBACKEND AS global_entity_id,\
-                        VBAK.BSTNK AS order_id,\
-                        VBAK.ZTIMESTAMP AS created_at,\
-                        VBAK.UPD_TMSTMP AS updated_at,\
-                        (0-VBRP.NETWR) AS vendor_net_revenue,\
-                        VBAK.ZPAYMENTPROVIDER AS provider,\
-                        VBAK.VBELN,\
-                        vbak.KNUMV,\
-                        VBRK.MWSBK as tax_charge\
-                        FROM VBAK\
-                        left JOIN VBRP ON VBAK.VBELN  = VBRP.AUBEL\
-                        left JOIN VBRK ON VBRP.VBELN  = VBRK.VBELN\
-                        WHERE VBAK.BSTNK = 'xyke-a66l'\
-                        ORDER by VBRP.VBELN desc"
-                       )
+        for order_id in order_list:
+            # VBRP can have multyple records
+            cursor.execute("SELECT VBAK.ZBACKEND AS global_entity_id,\
+                            VBAK.BSTNK AS order_id,\
+                            VBAK.ZTIMESTAMP AS created_at,\
+                            VBAK.UPD_TMSTMP AS updated_at,\
+                            (0-VBRP.NETWR) AS vendor_net_revenue,\
+                            VBAK.ZPAYMENTPROVIDER AS provider,\
+                            VBAK.VBELN,\
+                            vbak.KNUMV,\
+                            VBRK.MWSBK as tax_charge\
+                            FROM VBAK\
+                            left JOIN VBRP ON VBAK.VBELN  = VBRP.AUBEL\
+                            left JOIN VBRK ON VBRP.VBELN  = VBRK.VBELN\
+                            WHERE VBAK.BSTNK = '" + order_id[0] + "'\
+                            ORDER by VBRP.VBELN desc"
+                           )
 
-        # for num, row in enumerate(cursor):
-        row = cursor.fetchone()
+            # for num, row in enumerate(cursor):
+            row = cursor.fetchone()
 
-        data['global_entity_id'] = row[0]
-        data['order_id'] = row[1]
-        data['created_at'] = row[2]
-        data['updated_at'] = str(row[3])
-        data['vendor_net_revenue'] = str(row[4])
+            data['global_entity_id'] = row[0]
+            data['order_id'] = row[1]
+            data['created_at'] = row[2]
+            data['updated_at'] = str(row[3])
+            data['vendor_net_revenue'] = str(row[4])
 
-        data['payout'] = {}
-        data['subtotal'] = {}
-        data['payment'] = {}
-        data['discount'] = []
-        data['delivery'] = {}
-        data['voucher'] = {}
-        data['commission'] = {}
-        data['joker'] = {}
-        data['customer_refund'] = {}
-        data['vendor_refund'] = {}
-        data['customer_fee'] = {}
-        data['tip'] = {}
-        data['tax'] = {}
-        data['vendor_charges'] = {}
+            data['payout'] = {}
+            data['subtotal'] = {}
+            data['payment'] = {}
+            data['discount'] = []
+            data['delivery'] = {}
+            data['voucher'] = {}
+            data['commission'] = {}
+            data['joker'] = {}
+            data['customer_refund'] = {}
+            data['vendor_refund'] = {}
+            data['customer_fee'] = {}
+            data['tip'] = {}
+            data['tax'] = {}
+            data['vendor_charges'] = {}
 
-        data['payment']['provider'] = row[5]
-        data['tax']['tax_charge'] = str(row[8])
-        VBAK_VBELN = row[6]
-        VBAK_KNUMV = row[7]
+            data['payment']['provider'] = row[5]
+            data['tax']['tax_charge'] = str(row[8])
+            VBAK_VBELN = row[6]
+            VBAK_KNUMV = row[7]
 
-        discount_vendor = {}
-        discount_plaform = {}
-        ZVAM = 0
-        ZVA2 = 0
-        ZVA3 = 0
+            discount_vendor = {}
+            discount_plaform = {}
+            ZVAM = 0
+            ZVA2 = 0
+            ZVA3 = 0
 
-        cursor.execute("SELECT KSCHL, KWERT FROM PRCD_ELEMENTS pe \
-                WHERE pe.KNUMV  = '" + VBAK_KNUMV + "'"
-                       )
+            cursor.execute("SELECT KSCHL, KWERT FROM PRCD_ELEMENTS pe \
+                    WHERE pe.KNUMV  = '" + VBAK_KNUMV + "'"
+                           )
 
-        pe = cursor.fetchall()
-        for KSCHL, KWERT in pe:
-            match KSCHL:
-                case 'Z052':
-                    data['customer_paid_amount'] = str(0 - KWERT)
-                case 'Z04C':
-                    data['subtotal']['gross_amount'] = str(KWERT)
-                case 'Z022':
-                    data['subtotal']['net_amount'] = str(KWERT)
-                case 'Z04G':
-                    discount_vendor['gross_food_amount'] = str(KWERT)
-                case 'Z062':
-                    discount_vendor['net_food_amount'] = str(KWERT)
-                case 'Z04L':
-                    discount_vendor['gross_delivery_amount'] = str(KWERT)
-                case 'Z04K':
-                    discount_vendor['net_delivery_amount'] = str(KWERT)
-                case 'Z04F':
-                    discount_plaform['gross_food_amount'] = str(KWERT)
-                case 'Z064':
-                    discount_plaform['net_food_amount'] = str(KWERT)
-                case 'Z079':
-                    discount_plaform['gross_delivery_amount'] = str(KWERT)
-                case 'Z078':
-                    discount_plaform['net_delivery_amount'] = str(KWERT)
-                case 'Z04D':
-                    data['delivery']['gross_fee'] = str(KWERT)
-                case 'ZDF1':
-                    data['subtotal']['net_fee'] = str(KWERT)
-                case 'Z075':
-                    data['voucher']['gross_amount'] = str(KWERT)
-                    data['voucher']['owner'] = 'Vendor'
-                case 'ZVO1':
-                    data['voucher']['net_amount'] = str(KWERT)
-                case 'Z077':
-                    data['voucher']['gross_amount'] = str(KWERT)
-                    data['voucher']['owner'] = 'Platform'
-                case 'Z076':
-                    data['voucher']['net_amount'] = str(KWERT)
-                case 'ZJF1':
-                    data['jocker']['gross_fee'] = str(KWERT)
-                case 'ZJF2':
-                    data['jocker']['net_fee'] = str(KWERT)
-                case 'ZSFG':
-                    data['customer_fee']['gross_service_fee'] = str(KWERT)
-                case 'ZSFN':
-                    data['customer_fee']['net_service_fee'] = str(KWERT)
-                case 'Z024':
-                    data['customer_fee']['gross_container_fee'] = str(KWERT)
-                case 'Z04E':
-                    data['customer_fee']['gross_mov_fee'] = str(KWERT)
-                case 'ZMV0':
-                    data['customer_fee']['net_mov_fee'] = str(KWERT)
-                case 'ZVAM':
-                    ZVAM = KWERT
-                case 'ZVA2':
-                    ZVA2 = KWERT
-                case 'ZVA3':
-                    ZVA3 = KWERT
+            pe = cursor.fetchall()
+            for KSCHL, KWERT in pe:
+                match KSCHL:
+                    case 'Z052':
+                        data['customer_paid_amount'] = str(0 - KWERT)
+                    case 'Z04C':
+                        data['subtotal']['gross_amount'] = str(KWERT)
+                    case 'Z022':
+                        data['subtotal']['net_amount'] = str(KWERT)
+                    case 'Z04G':
+                        discount_vendor['gross_food_amount'] = str(KWERT)
+                    case 'Z062':
+                        discount_vendor['net_food_amount'] = str(KWERT)
+                    case 'Z04L':
+                        discount_vendor['gross_delivery_amount'] = str(KWERT)
+                    case 'Z04K':
+                        discount_vendor['net_delivery_amount'] = str(KWERT)
+                    case 'Z04F':
+                        discount_plaform['gross_food_amount'] = str(KWERT)
+                    case 'Z064':
+                        discount_plaform['net_food_amount'] = str(KWERT)
+                    case 'Z079':
+                        discount_plaform['gross_delivery_amount'] = str(KWERT)
+                    case 'Z078':
+                        discount_plaform['net_delivery_amount'] = str(KWERT)
+                    case 'Z04D':
+                        data['delivery']['gross_fee'] = str(KWERT)
+                    case 'ZDF1':
+                        data['subtotal']['net_fee'] = str(KWERT)
+                    case 'Z075':
+                        data['voucher']['gross_amount'] = str(KWERT)
+                        data['voucher']['owner'] = 'Vendor'
+                    case 'ZVO1':
+                        data['voucher']['net_amount'] = str(KWERT)
+                    case 'Z077':
+                        data['voucher']['gross_amount'] = str(KWERT)
+                        data['voucher']['owner'] = 'Platform'
+                    case 'Z076':
+                        data['voucher']['net_amount'] = str(KWERT)
+                    case 'ZJF1':
+                        data['joker']['gross_fee'] = str(KWERT)
+                    case 'ZJF2':
+                        data['joker']['net_fee'] = str(KWERT)
+                    case 'ZSFG':
+                        data['customer_fee']['gross_service_fee'] = str(KWERT)
+                    case 'ZSFN':
+                        data['customer_fee']['net_service_fee'] = str(KWERT)
+                    case 'Z024':
+                        data['customer_fee']['gross_container_fee'] = str(
+                            KWERT)
+                    case 'Z04E':
+                        data['customer_fee']['gross_mov_fee'] = str(KWERT)
+                    case 'ZMV0':
+                        data['customer_fee']['net_mov_fee'] = str(KWERT)
+                    case 'ZVAM':
+                        ZVAM = KWERT
+                    case 'ZVA2':
+                        ZVA2 = KWERT
+                    case 'ZVA3':
+                        ZVA3 = KWERT
 
-        data['tax']['total_amount'] = str(ZVAM + ZVA2 + ZVA3)
+            data['tax']['total_amount'] = str(ZVAM + ZVA2 + ZVA3)
 
-        if 'gross_delivery_amount' in discount_vendor:
-            discount_vendor['owner'] = 'Vendor'
-            data['discount'].append(discount_vendor)
+            if 'gross_delivery_amount' in discount_vendor:
+                discount_vendor['owner'] = 'Vendor'
+                data['discount'].append(discount_vendor)
 
-        if 'gross_delivery_amount' in discount_plaform:
-            discount_vendor['owner'] = 'Platform'
-            data['discount'].append(discount_plaform)
+            if 'gross_delivery_amount' in discount_plaform:
+                discount_vendor['owner'] = 'Platform'
+                data['discount'].append(discount_plaform)
 
-        json_data = json.dumps(data, sort_keys=False, indent=4)
-        print(json_data)
+            json_data = json.dumps(data, sort_keys=False, indent=4)
+            f = open("output/" + order_id[0] + ".json", "a")
+            f.write(json_data)
+            f.close()
+            # print(json_data)
     except:
         logging.exception('HANA DB connection error')
 
